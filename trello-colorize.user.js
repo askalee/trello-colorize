aa72a2fdfdd3984fdd43f8aa84659109643f3e90
askalee <askalee@users.noreply.github.com>
2019-03-09 16:36:57 +0800

    Only red and green background colors

--- trello-colorize.user.js
+++ trello-colorize.user.js
@@ -1,30 +1,21 @@
 // ==UserScript==
 // @name       Trello card colorizer
-// @namespace  http://github.com/grimradical/trello-colorize
-// @version    0.2
+// @namespace  https://github.com/askalee/trello-colorize
+// @version    0.3
 // @require       http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js
-// @description  Changes background color of Trello cards to match labels
-// @match      https://trello.com/board/*
-// @copyright  2012+, Deepak Giridharagopal
+// @description  Changes background color of Trello cards to match only red/green labels
+// @match      https://trello.com/b/*
+// @copyright  2012+, Based on Deepak Giridharagopal's work
 // ==/UserScript==
 
 var colorize = function() {
     $(".list-card").css("background-color", "").css("border", "");
-    // Blocked
-    $(".list-card:has(.purple-label)").css("background-color", "#cc99ff");
-    // Slurm
-    $(".list-card:has(.green-label)").css("background-color", "#99cc66");
-    // Community-related
-    $(".list-card:has(.yellow-label)").css("background-color", "#ffff99");
-    // ???
-    $(".list-card:has(.orange-label)").css("background-color", "#ff9966");
+    // Story
+    $(".list-card:has(.card-label-green)").css("background-color", "#bce89d");
     // Bug
-    $(".list-card:has(.red-label)").css("background-color", "#ff9999");
-    // Feature
-    $(".list-card:has(.blue-label)").css("background-color", "#99ccff");
-    // Special visual marker for blocked cards
-    $(".list-card:has(.purple-label)").css("border", "7px solid purple");
+    $(".list-card:has(.card-label-red)").css("background-color", "#ffbbbb");
 };
 $(document).ready(function() {
+  colorize();
   setInterval(colorize, 10000);
 });
